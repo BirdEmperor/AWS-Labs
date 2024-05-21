6edc522689190047b2d136c630c2c0f5b17ba54e
Bohdan Husiev, RI-11sp <bohdan.husiev.ri.2023@lpnu.ua>
2024-05-21 03:28:18 +0300

    try to fix this ******* ****

--- apigtw.tf
+++ apigtw.tf
@@ -1,24 +1,24 @@
 resource "aws_api_gateway_rest_api" "this" {
-#     body = jsonencode({
-#   "openapi": "3.0.1",
-#   "info": {
-#     "title": "example",
-#     "version": "1.0"
-#   },
-#   "paths": {
-#     "/path1": {
-#       "get": {
-#         "x-amazon-apigateway-integration": {
-#           "httpMethod": "GET",
-#           "payloadFormatVersion": "1.0",
-#           "type": "HTTP_PROXY",
-#           "uri": "https://ip-ranges.amazonaws.com/ip-ranges.json"
-#         }
-#       }
-#     }
-#   }
-# }
-# )
+    body = jsonencode({
+  "openapi": "3.0.1",
+  "info": {
+    "title": "example",
+    "version": "1.0"
+  },
+  "paths": {
+    "/path1": {
+      "get": {
+        "x-amazon-apigateway-integration": {
+          "httpMethod": "GET",
+          "payloadFormatVersion": "1.0",
+          "type": "HTTP_PROXY",
+          "uri": "https://ip-ranges.amazonaws.com/ip-ranges.json"
+        }
+      }
+    }
+  }
+}
+)
 
   name = module.label_api.id
   description = "API Gateway"
